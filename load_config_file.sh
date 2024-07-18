e15c079a836825e1326ec4d95606aa1bc4de50d7
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2024-07-18 17:37:34 +0200

    Added changelog_script_url file option

--- load_config_file.sh
+++ load_config_file.sh
@@ -18,12 +18,13 @@ if [ -z $configFileIncluded ]; then
 	fi
 
 	declare -A params=()
-	declare -a knownOptions=("identity" "signtool_options" "notarization_profile" "use_sparkle" "appcast_releases" "appcast_betas" "appcast_releases_fallback" "appcast_betas_fallback" "symbols_symstore_path" "symbols_windows_pdb_server_path" "symbols_macos_dsym_server_path")
+	declare -a knownOptions=("identity" "signtool_options" "notarization_profile" "use_sparkle" "changelog_script_url" "appcast_releases" "appcast_betas" "appcast_releases_fallback" "appcast_betas_fallback" "symbols_symstore_path" "symbols_windows_pdb_server_path" "symbols_macos_dsym_server_path")
 
 	# Default values
 	params["identity"]=""
 	params["notarization_profile"]=""
 	params["use_sparkle"]=false
+	params["changelog_script_url"]=""
 	params["appcast_releases"]="https://localhost/appcast-release.xml"
 	params["appcast_betas"]="https://localhost/appcast-beta.xml"
 	params["appcast_releases_fallback"]="https://localhost/appcast-release.xml"
